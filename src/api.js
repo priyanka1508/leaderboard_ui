f7041d81eb0c8ef96e110387c52d1499d57c4d32
priyanka1508 <priyankaphulwari16@gmail.com>
2025-07-24 01:06:22 +0530

    new url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const localhost_url = 'http://localhost:8000';
-const host_url = 'https://leaderboard-backend-snowy.vercel.app';
+const host_url = process.env.REACT_APP_API_BASE_URL;
 
 const API = axios.create({
   baseURL: `${host_url}/api/leaderboard`,
