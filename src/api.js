06962582173caa5c87b5dd68ee56abe0336dd03e
priyanka1508 <priyankaphulwari16@gmail.com>
2025-07-24 00:51:32 +0530

    url change

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const localhost_url = 'http://localhost:8000';
-const host_url = 'https:/leaderboard-backend-snowy.vercel.app';
+const host_url = 'https://leaderboard-backend-snowy.vercel.app';
+
 const API = axios.create({
   baseURL: `${host_url}/api/leaderboard`,
 });
