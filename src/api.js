df2b0bcab3c171211bcd941c38c393218cf307f3
priyanka1508 <priyankaphulwari16@gmail.com>
2025-07-24 23:13:09 +0530

    sanitizing the url

--- src/api.js
+++ src/api.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
+function normalizeUrl(url) {
+  return url.replace(/([^:]\/)\/+/g, "$1");
+}
+
+
 const localhost_url = 'http://localhost:8000';
 const host_url = process.env.REACT_APP_API_BASE_URL;
-
+console.log('host_url', host_url);
 const API = axios.create({
-  baseURL: `${host_url}/api/leaderboard`,
+  baseURL: normalizeUrl(`${host_url}/api/leaderboard`),
 });
 
 export default API;
